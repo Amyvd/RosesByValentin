1c5fa98290593caf22c7533cb3e210a89b76d809
amyvd <70899521+Amyvd@users.noreply.github.com>
2022-06-02 01:25:17 +0200

    update

--- aboutme.php
+++ aboutme.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Roses By Valentin</title>
-
-    <!-- Style Sheets -->
-    <link rel="stylesheet" href="css/landingspage.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <title>Document</title>
     
+    <!-- style documents -->
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/menuoverlay.css">
-  <script src="javascript files/mobilenav.js"></script>
+
+    <script src="javascript files/mobilenav.js"></script>
 </head>
 <body>
-    <div class="container">
+<div class="container">
         <div class="navigationbar">
             <div class="logo">
                 <h2>Roses By Valentin</h2>
@@ -47,46 +47,7 @@
             </div>
         </div>
     </div>
-
-    <!-- Featured Roses Section -->
-    <div class="featuredRoses"id="featuredRoses">
-        <h1>Featured Roses</h1>
-        <div class="featuredSection">
-            <div class="roosbox1">
-                <img src="afbeeldingen/roos1.png" alt="" class="testroos">
-                <h3 class="roosboxText">Rose Box 1</h3>
-                <p class="boxText1">$160,00</p>
-                <a href="#featuredRoses" class="buttonBox box1">Buy Now</a>
-            </div>
-            <div class="roosbox2">
-                <img src="afbeeldingen/roos1.png" alt="" class="testroos">
-                <h3 class="roosboxText">Rose Box 2</h3>
-                <p class="boxText1">$160,00</p>
-                <a href="#featuredRoses" class="buttonBox box2">Buy Now</a>
-            </div>
-            <div class="roosbox3">
-                <img src="afbeeldingen/roos1.png" alt="" class="testroos">
-                <h3 class="roosboxText">Rose Box 3</h3>
-                <p class="boxText1">$160,00</p>
-                <a href="#featuredRoses" class="buttonBox box3">Buy Now</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="whitespace"></div>
-
-    <div class="nieuwsbriefsection">
-        <h2>Nieuwsbrief</h2>
-        <p>Wil je lid worden van nieuwsbrief. Meld je dan aan met je Email Adress</p>
-        <form action=" ">
-            <input type="email" placeholder="Email adress" class="formEmail"></input>
-            <button type="submit">Verzend</button>
-        </form>
-    </div>
-
-    <div class="greenspace"></div>
-
-    <footer>
+<footer>
         <div class="footerGrid">
             <div class="footerGridOne">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2047.4612622413852!2d4.855316759410424!3d52.391169770175104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5e2884124ea7f%3A0xf2fdbed6bb7875cf!2sContactweg%2036%2C%201014%20AN%20Amsterdam!5e0!3m2!1snl!2snl!4v1653920957576!5m2!1snl!2snl" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="googleMaps"></iframe>
@@ -105,10 +66,8 @@
                     <h3>NieuwsBrief</h3>
                     <p>Wil je lid worden van nieuwsbrief. 
                     Meld je dan aan met je Email Adress</p>
-                   <form action="">
-                        <input type="email" name="emailadress" id="" placeholder="email" >
-                        <button type="submit" class="footersubmitButton">submit</button>
-                   </form>
+                    <input type="email" name="emailadress" id="" placeholder="email" >
+                    <button type="submit" class="footersubmitButton">submit</button>
                 </div>
                 <div class="socialIcons">
                     <a href=""><img src="afbeeldingen/email.png" alt="" class="emailIcn"></a>
@@ -119,6 +78,5 @@
             </div>
         </div>
     </footer>
-
-</body>     
-</html>    
+</body>
+</html>
